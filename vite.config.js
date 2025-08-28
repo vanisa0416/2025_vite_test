91f1833cafb05e95921d52909b28cce9d74e087d
vanisa0416 <vanisa0416@msn.com>
2025-08-28 23:06:45 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/2025_vite_test/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
